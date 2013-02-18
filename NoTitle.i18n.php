d93a5fd23e6c202b1d963c621cfe8ac012f32a60
raymond <raimond.spekking@gmail.com>
2013-02-18 22:38:57 +0100

    Consistency tweaks in preparation for adding extension to Translatewiki.net
    * Remove unneccessary guard
    * Remove period from end of description message
    
    Change-Id: I85833dcdf364af89e50fac1c7f883a6b9d76b76e

--- NoTitle.i18n.php
+++ NoTitle.i18n.php
@@ -1,15 +1,11 @@
 <?php
-/*
+/**
  *  NoTitle
  *  Adds a magic word that hides the main title heading in a page
  *
  * @file NoTitle.i18n.php
  * @author Tony Boyles
  */
- 
-if (!defined('MEDIAWIKI')) {
-	die('This file is an extension to the <a href=\'http://www.mediawiki.org/\'>MediaWiki Platform</a> and cannot be used standalone.');
-}
 
 $messages = array();
 
@@ -17,7 +13,7 @@ $messages = array();
  * @author Tony Boyles
  */
 $messages['en'] = array(
-	'notitle-desc' => 'Adds a magic word to hide the title heading.'
+	'notitle-desc' => 'Adds a magic word to hide the title heading'
 );
 
 /** Message documentation (Message documentation)
@@ -26,3 +22,4 @@ $messages['en'] = array(
 $messages['qqq'] = array(
 	'notitle-desc' => '{{desc}}',
 );
+
