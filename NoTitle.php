bdab198fc3b11a151a8b02a84277d1be9243b8e9
Karsten Hoffmeyer <karsten@hoffmeyer.info>
2017-07-25 10:33:53 +0200

    Remove I18n php shim and bump version
    Also
    
    * Tidy main file
    * Extend file documentation
    * Update author's array
    
    Note: This is breaking for MW 1.22.x and earlier.
    
    Bug:T168353
    Change-Id: Iaaf797499ce424bf97dbd614b2b07f9f62f5590a

--- NoTitle.php
+++ NoTitle.php
@@ -1,37 +1,49 @@
 <?php
 /*
- *  NoTitle
- *  Adds a magic word that hides the main title heading in a page
+ * NoTitle extension to MediaWiki
+ *
+ * Adds a magic word that hides the main title heading in a page
+ *
+ * @link https://www.mediawiki.org/wiki/Extension:NoTitle Documentation
+ * @link https://www.mediawiki.org/wiki/Extension_talk:NoTitle Support
+ * @link https://phabricator.wikimedia.org/diffusion/ENOT/ Source code
  *
  * @file NoTitle.php
+ * @ingroup Extensions
+ * @package MediaWiki
+ *
  * @author Carlo Cabanilla
  * @author Tony Boyles
+ *
+ * @license https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License 3.0 or later
  */
 
+// Ensure that the script cannot be executed outside of MediaWiki.
 if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'This file is an extension to the <a href="https://www.mediawiki.org/">MediaWiki platform</a> and cannot be used standalone.' );
+	die( 'This file is an extension to MediaWiki and cannot be used standalone.' );
 }
 
-# File inclusion and registration:
-$wgAutoloadClasses['NoTitle'] = __DIR__ . '/NoTitle.body.php';
-$wgMessagesDirs['NoTitle'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['NoTitle'] = __DIR__ . '/NoTitle.i18n.php';
-$wgExtensionMessagesFiles['NoTitleMagic'] = __DIR__ . '/NoTitle.i18n.magic.php';
-
-# Hook our events:
-$wgHooks['ParserBeforeTidy'][] = 'NoTitle::killTitle';
-
-# Extension Registration:
+// Display extension properties on MediaWiki.
 $wgExtensionCredits['parserhook'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'NoTitle',
-	'version'        => '0.2.0',
-	'author'         => array(
+	'path' => __FILE__,
+	'name' => 'NoTitle',
+	'version' => '0.3.0',
+	'author' => array(
 		'Carlo Cabanilla',
-		'[http://milcord.com/ Tony Boyles, Milcord LLC]'
+		'Tony Boyles',
+		'...'
 	),
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:NoTitle',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:NoTitle',
 	'descriptionmsg' => 'notitle-desc',
-	'license-name'   => 'GPL-3.0+'
+	'license-name' => 'GPL-3.0+'
 );
 
+// Load extension classes.
+$wgAutoloadClasses['NoTitle'] = __DIR__ . '/NoTitle.body.php';
+
+// Register extension messages and other localisation.
+$wgMessagesDirs['NoTitle'] = __DIR__ . '/i18n';
+$wgExtensionMessagesFiles['NoTitleMagic'] = __DIR__ . '/NoTitle.i18n.magic.php';
+
+// Register extension hooks.
+$wgHooks['ParserBeforeTidy'][] = 'NoTitle::killTitle';
