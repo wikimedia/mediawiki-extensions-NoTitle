1cf4475470da6fe0c1f62550e6ba869797eb3a74
Jayprakash12345 <0freerunning@gmail.com>
2018-01-20 05:07:13 +0000

    Convert NoTitle to use extension registration
    Bug: T185370
    Change-Id: I54f9d0c2369d10ec057f278dfc3e6f63d18802e3

--- NoTitle.php
+++ NoTitle.php
@@ -18,32 +18,16 @@
  * @license https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License 3.0 or later
  */
 
-// Ensure that the script cannot be executed outside of MediaWiki.
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'This file is an extension to MediaWiki and cannot be used standalone.' );
+ if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'NoTitle' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['NoTitle'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the NoTitle extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the NoTitle extension requires MediaWiki 1.29+' );
 }
-
-// Display extension properties on MediaWiki.
-$wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'NoTitle',
-	'version' => '0.3.0',
-	'author' => array(
-		'Carlo Cabanilla',
-		'Tony Boyles',
-		'...'
-	),
-	'url' => 'https://www.mediawiki.org/wiki/Extension:NoTitle',
-	'descriptionmsg' => 'notitle-desc',
-	'license-name' => 'GPL-3.0+'
-);
-
-// Load extension classes.
-$wgAutoloadClasses['NoTitle'] = __DIR__ . '/NoTitle.body.php';
-
-// Register extension messages and other localisation.
-$wgMessagesDirs['NoTitle'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['NoTitleMagic'] = __DIR__ . '/NoTitle.i18n.magic.php';
-
-// Register extension hooks.
-$wgHooks['ParserBeforeTidy'][] = 'NoTitle::killTitle';
